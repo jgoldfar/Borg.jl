ee8a832769f7ee53f7f07d555a63a33ff95abceb
Jonathan Goldfarb <jgoldfar@gmail.com>
2016-03-08 09:45:22 -0500

    Replace mutable types by immutables, and eval_wrapper outer function with closure; much cleaner this way (but does not work on master)

--- src/Borg.jl
+++ src/Borg.jl
@@ -3,77 +3,56 @@ module Borg
 
 include("BorgC.jl")
 
-export createProblem, solve
+export BorgProblem, solve
 
-function eval_wrapper(vars_raw::Ptr{Float64}, objs_raw::Ptr{Float64}, consts_raw::Ptr{Float64}, userParams::Ptr{Void})
-    problem = unsafe_pointer_to_objref(userParams)::BorgProblem
-
-    vars = pointer_to_array(vars_raw, problem.numberOfVariables)
-    objs = pointer_to_array(objs_raw, problem.numberOfObjectives)
-    consts = pointer_to_array(consts_raw, problem.numberOfConstraints)
-
-    problem.eval_fn(vars, objs, consts)
-
-    return
+immutable BorgProblemData
+  numberOfVariables::Int64
+  numberOfObjectives::Int64
+  numberOfConstraints::Int64
+  eval_fn::Function
 end
-
-type BorgProblem
-	ref::BorgC.BORG_Problem
-
-	numberOfVariables::Int64
-	numberOfObjectives::Int64
-	numberOfConstraints::Int64
-
-	eval_fn::Function
-
-	function BorgProblem(
-            numberOfVariables::Int,
-            numberOfObjectives::Int,
-            numberOfConstraints::Int,
-            eval_fn::Function)
-
-		problem = new()
-
-        problem.numberOfVariables = numberOfVariables
-        problem.numberOfObjectives = numberOfObjectives
-        problem.numberOfConstraints = numberOfConstraints
-        problem.eval_fn = eval_fn
-		problem.ref = BorgC.BORG_Problem_create(numberOfVariables, numberOfObjectives, numberOfConstraints, eval_wrapper, problem)
-
-        return problem
-	end
+immutable BorgProblem
+  ref::BORG_Problem
+  data::BorgProblemData
 end
+function BorgProblem(
+  varLowerBounds::Vector{Float64},
+  varUpperBounds::Vector{Float64},
+  objEpsilons::Vector{Float64},
+  numberOfConstraints::Int,
+  eval_fn::Function)
 
-function createProblem(
-    varLowerBounds::Vector{Float64},
-    varUpperBounds::Vector{Float64},
-    objEpsilons::Vector{Float64},
-    numberOfConstraints::Int,
-    eval_fn::Function)
+  numberOfVariables = length(varLowerBounds)
+  if numberOfVariables != length(varUpperBounds)
+    error("varLowerBounds and varUpperBounds must be of same length.")
+  end
 
-    if length(varLowerBounds) != length(varUpperBounds)
-        error("varLowerBounds and varUpperBounds must be of same length.")
-    end
+  numberOfObjectives = length(objEpsilons)
 
-    numberOfVariables = length(varLowerBounds)
-    numberOfObjectives = length(objEpsilons)
+  problem = BorgProblemData(numberOfVariables, numberOfObjectives, numberOfConstraints, eval_fn)
 
-    problem = BorgProblem(numberOfVariables, numberOfObjectives, numberOfConstraints, eval_fn)
+  function eval_wrapper(vars_raw::Ptr{Float64}, objs_raw::Ptr{Float64}, consts_raw::Ptr{Float64}, userParams::Ptr{Void})
+    vars = pointer_to_array(vars_raw, numberOfVariables)
+    objs = pointer_to_array(objs_raw, numberOfObjectives)
+    consts = pointer_to_array(consts_raw, numberOfConstraints)
+    eval_fn(vars, objs, consts)
+    return nothing
+  end
+  problem_ref = BORG_Problem_create(numberOfVariables, numberOfObjectives, numberOfConstraints, eval_wrapper, Ptr{Void})
 
-    for i in 1:numberOfVariables
-        BorgC.BORG_Problem_set_bounds(problem.ref, i, varLowerBounds[i], varUpperBounds[i])
-    end
+  for i in 1:numberOfVariables
+    BORG_Problem_set_bounds(problem_ref, i, varLowerBounds[i], varUpperBounds[i])
+  end
 
-    for i in 1:numberOfObjectives
-        BorgC.BORG_Problem_set_epsilon(problem.ref, i, objEpsilons[i])
-    end
+  for i in 1:numberOfObjectives
+    BORG_Problem_set_epsilon(problem_ref, i, objEpsilons[i])
+  end
 
-    return problem
+  BorgProblem(problem_ref, problem)
 end
 
 function solve(problem::BorgProblem, maxIt::Int)
-    archive = BorgC.BORG_Algorithm_run(problem.ref, maxIt)
-    return archive
+  BORG_Algorithm_run(problem.ref, maxIt)
 end
 
 end
